48f8bd6cdd22ce0f92071bd4c18734295a37a4eb
wywangwei <wang1230yang1230@163.com>
2019-09-24 09:15:32 +0800

    product_show

--- resources/views/erp/product/show.blade.php
+++ resources/views/erp/product/show.blade.php
@@ -10,7 +10,7 @@
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">分类名称:</label>
+            <label class="layui-form-label">产品名称:</label>
             <div class="layui-input-block">
                 <div class="layui-form-mid layui-word-aux">{{$data->product_name}}</div>
             </div>
@@ -66,9 +66,9 @@
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">产品条形码:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->barcode}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
@@ -87,72 +87,72 @@
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">成本价:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_cost_price}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">销售价:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_price}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">库存报警:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_storage_alarm}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">产品运费:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_freight}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">产品尺寸:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_size}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">产品重量:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_weight}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">主供应商链接:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->url}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">辅供应商链接:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->supplier_burl}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">类型名称:</label>
+            <label class="layui-form-label">产品推荐:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->type_name}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_commend}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
         <div class="layui-form-item">
-            <label class="layui-form-label">排序:</label>
+            <label class="layui-form-label">产品状态:</label>
             <div class="layui-input-block">
-                <div class="layui-form-mid layui-word-aux">{{$data->sort}}</div>
+                <div class="layui-form-mid layui-word-aux">{{$data->product_state}}</div>
             </div>
         </div>
         <hr class="layui-bg-gray">
